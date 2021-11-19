c4f5101b7f056bb5f7810204e15dc67fe0b404d2
MissLiner <miss.liner@yahoo.com>
2021-11-19 12:00:34 -0500

    Make functioning makeDropdown

--- src/index.js
+++ src/index.js
@@ -1,7 +1,23 @@
 import './style.css';
 
-const content = document.getElementById("content");
-const greeting = document.createElement("div");
+const dropdownMenu = document.getElementById('dropdown-menu');
+const dropdownMenuBtn = document.getElementById('dropdown-menu-btn');
 
-greeting.textContent = "Good Morning, Caroline!";
-content.appendChild(greeting);
+function makeDropdown(button, menu) {
+    function show(divName) {
+        divName.classList.remove('hidden');
+    }
+    function hide(divName) {
+        divName.classList.add('hidden');
+    }
+
+    button.addEventListener('click', () => {
+        if (menu.classList.contains('hidden')) {
+            show(menu);
+        } else {
+            hide(menu);
+        }
+    })
+}
+
+makeDropdown(dropdownMenuBtn, dropdownMenu);
