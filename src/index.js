b919d28c8ea4acc005bfe7f965e53b9888ba8505
MissLiner <miss.liner@yahoo.com>
2021-11-19 11:22:45 -0500

    Add webpack and eslint

---
+++ src/index.js
@@ -0,0 +1,5 @@
+const content = document.getElementById("content");
+const greeting = document.createElement("div");
+
+greeting.textContent = "Good Morning, Caroline!";
+content.appendChild(greeting);
