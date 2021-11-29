c6dee6bde7a213ca827295f4d14237e4782ea762
MissLiner <miss.liner@yahoo.com>
2021-11-29 11:19:35 -0500

    Start adding mobile dropdown option

--- src/index.js
+++ src/index.js
@@ -1,7 +1,21 @@
 import './style.css';
-import { makeThisDropdown } from './dropdown';
+import { makeThisDropdown } from './dropdowns';
 
 const dropdownMenu = document.getElementById('dropdown-menu');
 const dropdownMenuBtn = document.getElementById('dropdown-menu-btn');
 
+const mobileWidth = window.matchMedia('(max-width: 480px)');
+function checkMedia(mediaQuery) {
+    if (mediaQuery.matches) {
+
+    } else {
+
+    }
+}
+
 makeThisDropdown(dropdownMenuBtn, dropdownMenu);
+
+//create mobile dropdown
+//create desktop dropdown
+//check which one it is
+//create appropriate dropdown
