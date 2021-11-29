afd3cd69eada294651726aa3894a76bc6921c0ca
MissLiner <miss.liner@yahoo.com>
2021-11-29 14:55:15 -0500

    Add functioning open menu btn to mobile menu, start fixing styles

--- src/dropdown_responsive.js
+++ src/dropdown_responsive.js
@@ -1,10 +1,11 @@
+function show(divName) {
+  divName.classList.remove('hidden');
+}
+function hide(divName) {
+  divName.classList.add('hidden');
+}
+
 function makeThisDropdownStd(button, menu) {
-  function show(divName) {
-      divName.classList.remove('hidden');
-  }
-  function hide(divName) {
-      divName.classList.add('hidden');
-  }
   function addClickOutListener(button, menu) {
     document.addEventListener(
       "click",
@@ -30,11 +31,18 @@ function makeThisDropdownStd(button, menu) {
   }
 }
 
-function setBtnStyle(button, menu) {
-  button.textContent = '+';
-  const childEls = Array.from(menu.children);
-  childEls.push(button);
-  childEls.forEach (element => {
+function setBtnStyle(div) {
+  const childEls1 = Array.from(div.children);
+  const childEls2 = [];
+  childEls1.forEach(element => {
+    if (div.children) {
+      const newArr = Array.from(div.children);
+      childEls2.concat(newArr);
+    }
+  })
+  childEls1.concat(childEls2);
+  //childEls.push(button);
+  childEls1.forEach (element => {
     element.style.color = 'white';
     element.style.backgroundColor = 'transparent';
     element.style.border = 'none';
@@ -47,14 +55,31 @@ function setMenuStyle(div, color) {
 }
 
 function makeThisDropdownMob(button, menu, div, color) {
-  setBtnStyle(button, menu);
+  //ADD OPEN MENU ICON, HIDE MAIN BUTTON
+  const openMenuBtn = document.createElement('div');
+  div.appendChild(openMenuBtn);
+  openMenuBtn.textContent = '+';
+  openMenuBtn.addEventListener('click', () => {
+    if (button.classList.contains('hidden')) {
+      show(button);
+      show(menu);
+    } else {
+      hide(button);
+      hide(menu);
+    }
+  })
+  setBtnStyle(div);
+  setBtnStyle(menu);
   setMenuStyle(div, color);
+  hide(button);
 }
 
 function chooseDropdown(button, menu, div, isMobile, color) {
-  makeThisDropdownStd(button, menu);
+  
   if (isMobile === true) {
-      makeThisDropdownMob(button, menu, div, color);
+    makeThisDropdownMob(button, menu, div, color);
+  } else {
+    makeThisDropdownStd(button, menu);
   }
 }
 
