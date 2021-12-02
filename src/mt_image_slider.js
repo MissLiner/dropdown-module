2678e599696e7afd96a6901bd0ee2dd09eb15b74
MissLiner <miss.liner@yahoo.com>
2021-12-02 12:03:35 -0500

    Add class-based transitions, not working

--- src/mt_image_slider.js
+++ src/mt_image_slider.js
@@ -9,27 +9,51 @@ function mtImageSlider(frame, element, leftBtn, rightBtn, picArray) {
     });
     picArray[currentIndex].style.display = 'initial';
 
-    function shiftSlider(direction) {
-        picArray[currentIndex].style.display = 'none';
+    function shiftSlider(direction, element) {
+        function toggleDisplay(element) {
+            if (element.style.display === 'none') {
+                element.style.display = 'initial';
+            } else {
+                element.style.display = 'none';
+            }
+        }
+        function toggleEffect(element, effect) {
+            element.classList.add(effect);
+            //let timeout = 3000;
+            setTimeout(() => {
+                element.classList.remove(effect);
+            }, 3000);
+            // if (element.classList.contains(effect)) {
+            //     element.classList.remove(effect);
+            // } else {
+            //     element.classList.add(effect);
+            // }
+        }
+        function transition(element, effect) {
+            toggleDisplay(element);
+            toggleEffect(element, effect);
+        }
+        transition(picArray[currentIndex], 'hide-effect')
         if (direction === 'left') {
             currentIndex -= 1;
         } else {
             currentIndex += 1;
         }
-        picArray[currentIndex].style.display = 'initial';
+        transition(picArray[currentIndex], 'show-effect')
+        //picArray[currentIndex].style.display = 'initial';
         const whiteSpace = frame.scrollWidth - picArray[currentIndex].width;
         if (whiteSpace > 0) {
             picArray[currentIndex].style.marginLeft = whiteSpace / 2;
         }
-        }
+    }
     leftBtn.addEventListener('click', () => {
         if (currentIndex !== 0) {
-            shiftSlider('left');
+            shiftSlider('left', picArray[currentIndex]);
         }
     })
     rightBtn.addEventListener('click', () => {
         if (currentIndex !== picArray.length - 1) {
-            shiftSlider('right');
+            shiftSlider('right', picArray[currentIndex]);
         }
     })
 }
