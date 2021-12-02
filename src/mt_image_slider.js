fdb7e28631f1f3c68a96ebe19fac897d034007b4
MissLiner <miss.liner@yahoo.com>
2021-12-02 16:21:32 -0500

    Add fade out and fade in effects

--- src/mt_image_slider.js
+++ src/mt_image_slider.js
@@ -9,51 +9,63 @@ function mtImageSlider(frame, element, leftBtn, rightBtn, picArray) {
     });
     picArray[currentIndex].style.display = 'initial';
 
-    function shiftSlider(direction, element) {
-        function toggleDisplay(element) {
-            if (element.style.display === 'none') {
-                element.style.display = 'initial';
-            } else {
-                element.style.display = 'none';
-            }
-        }
-        function toggleEffect(element, effect) {
-            element.classList.add(effect);
-            //let timeout = 3000;
+    function shiftSlider(direction, arr) {
+        function toggleDisplay(arr) {
             setTimeout(() => {
-                element.classList.remove(effect);
-            }, 3000);
-            // if (element.classList.contains(effect)) {
-            //     element.classList.remove(effect);
-            // } else {
-            //     element.classList.add(effect);
-            // }
+                if (arr[currentIndex].style.display === 'none') {
+                    arr[currentIndex].style.display = 'initial';
+                } else {
+                    arr[currentIndex].style.display = 'none';
+                }
+            }, 6000);
         }
-        function transition(element, effect) {
-            toggleDisplay(element);
-            toggleEffect(element, effect);
-        }
-        transition(picArray[currentIndex], 'hide-effect')
+        arr[currentIndex].classList.add('fade-out');
+        toggleDisplay(arr);
         if (direction === 'left') {
             currentIndex -= 1;
         } else {
             currentIndex += 1;
         }
-        transition(picArray[currentIndex], 'show-effect')
-        //picArray[currentIndex].style.display = 'initial';
+        toggleDisplay(arr);
         const whiteSpace = frame.scrollWidth - picArray[currentIndex].width;
         if (whiteSpace > 0) {
             picArray[currentIndex].style.marginLeft = whiteSpace / 2;
         }
+        arr[currentIndex].style.opacity = '0';
     }
+        // function toggleEffect(element, effect) {
+        //     element.classList.add(effect);
+        //     //let timeout = 3000;
+        //     setTimeout(() => {
+        //         element.classList.remove(effect);
+        //     }, 3000);
+            // if (element.classList.contains(effect)) {
+            //     element.classList.remove(effect);
+            // } else {
+            //     element.classList.add(effect);
+            // }
+        //}
+        // function transition(element, effect) {
+        //     toggleDisplay(element);
+        //     toggleEffect(element, effect);
+        // }
+        // transition(picArray[currentIndex], 'hide-effect')
+        // if (direction === 'left') {
+        //     currentIndex -= 1;
+        // } else {
+        //     currentIndex += 1;
+        // }
+        // transition(picArray[currentIndex], 'show-effect')
+        //picArray[currentIndex].style.display = 'initial';
+
     leftBtn.addEventListener('click', () => {
         if (currentIndex !== 0) {
-            shiftSlider('left', picArray[currentIndex]);
+            shiftSlider('left', picArray);
         }
     })
     rightBtn.addEventListener('click', () => {
         if (currentIndex !== picArray.length - 1) {
-            shiftSlider('right', picArray[currentIndex]);
+            shiftSlider('right', picArray);
         }
     })
 }
