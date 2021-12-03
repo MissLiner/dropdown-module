57e6a0f3949fdb2cd2acf4316c90dea07fdc108a
MissLiner <miss.liner@yahoo.com>
2021-12-02 21:13:30 -0500

    Get dissolve and appear working properly

--- src/mt_image_slider.js
+++ src/mt_image_slider.js
@@ -2,37 +2,39 @@ function mtImageSlider(frame, element, leftBtn, rightBtn, picArray) {
     // add pics to slider, display first pic
     picArray.forEach(pic => {
         element.append(pic);
-        pic.style.display = 'none';
+        pic.classList.add('hidden');
         pic.classList.add('slider-pic');
 
     });
     let currentIndex = 0;
-    //let newIndex = 1;
-    picArray[currentIndex].style.display = 'initial';
+    let newIndex;
+    picArray[currentIndex].classList.remove('hidden');
 
     function shiftSlider(direction, arr) {
         function dissolve(element) {
             element.classList.add('fade-out');
             function hide(element) {
                 element.classList.remove('fade-out');
-                element.style.display = 'none';
+                element.classList.add('hidden');
             }
             setTimeout(function() { hide(element) }, 3000);
         }
-        dissolve(arr[currentIndex]);
-        if (direction === 'left') {
-            currentIndex -= 1;
-            arr[currentIndex].style.display = 'initial';
+        function appear(element) {
+            function show(element) {
+                element.classList.remove('hidden');
+                element.classList.add('fade-in');
+            }
+            setTimeout(function() { show(element) }, 3000);
+        }
 
+        if (direction === 'left') {
+            newIndex = currentIndex - 1;
         } else {
-            currentIndex += 1;
-            arr[currentIndex].style.display = 'initial';
+            newIndex = currentIndex + 1; 
         }
-        const whiteSpace = frame.scrollWidth - picArray[currentIndex].width;
-        if (whiteSpace > 0) {
-            picArray[currentIndex].style.marginLeft = whiteSpace / 2;
-        }
-        //arr[currentIndex].style.opacity = '0';
+        dissolve(arr[currentIndex]);
+        appear(arr[newIndex]);
+        currentIndex = newIndex;
     }
     leftBtn.addEventListener('click', () => {
         if (currentIndex !== 0) {
